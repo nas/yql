ff53dcca5fb007ed63550871dbc7ccc493d6fee6
nas <nas35_in@yahoo.com>
2010-06-30 06:01:12 +0100

    Add specs for Yql::QueryBuilder.show_tables and .describe_table

--- spec/yql/query_builder_spec.rb
+++ spec/yql/query_builder_spec.rb
@@ -2,8 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Yql::QueryBuilder do
 
-  before(:each) do
-    
+  describe ".show_tables" do
+
+    it "should return show tables string" do
+      Yql::QueryBuilder.show_tables.should eql('show tables;')
+    end
+
   end
-  
+
+  describe ".describe_table" do
+
+    it "should return the describe tables query" do
+      Yql::QueryBuilder.describe_table('yql.table.name').should eql('desc yql.table.name;')
+    end
+
+  end
+
 end
